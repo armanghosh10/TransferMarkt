7f8bd809d5ee92b9c4d4d4ef31910c7addec04bc
armanghosh10 <149370200+armanghosh10@users.noreply.github.com>
2024-01-19 23:46:56 +0530

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,24 @@
+data = pd.read_excel('Final3.xlsx')
+data.columns
+
+#Selecting the input features used to make price predictions
+selected_columns = ['MV','Nation','Club','Leauge','Age','Starts','Gls90','Ast90','G+A','G/Sh',
+                    'Tackle','Succ_x','Blocks','Clr','Passes Completed','Cmp%','Touches']
+filtered_data = data[selected_columns]
+ 
+# Creating a instance of label Encoder.
+le = LabelEncoder()
+ 
+# Using .fit_transform function to fit label
+# encoder and return encoded label
+label1 = le.fit_transform(filtered_data['Nation'])
+label2 = le.fit_transform(filtered_data['Club'])
+label3 = le.fit_transform(filtered_data['Leauge'])
+
+filtered_data.drop("Nation", axis=1, inplace=True)
+filtered_data["Nation"] = label1
+filtered_data.drop("Club", axis=1, inplace=True)
+filtered_data["Club"] = label2
+filtered_data.drop("Leauge", axis=1, inplace=True)
+filtered_data["Leauge"] = label3
+filtered_data
